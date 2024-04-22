ee2cd23e4d98dbf0fcfa902cfc79b3efe2c4eb2d
Marko1310 <marko.cabo13@gmail.com>
2024-04-23 00:32:12 +0200

    Add tailwind custom variables, improve Homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,17 @@
-// import waveLight from '/wave-light.svg';
 import waveDark from '/wave-dark.svg';
 import blob from '/blob.svg';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <>
       <div
-        className="flex h-screen w-screen items-center bg-slate-50"
+        className="flex h-screen w-screen items-center"
         style={{
           backgroundImage: `url(${waveDark})`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
+          backgroundPosition: 'center',
         }}
       >
         <div className="flex h-full w-full items-center justify-between gap-12">
@@ -18,7 +19,7 @@ function Home() {
             <div className="flex h-full flex-col justify-between">
               <div className="flex h-full flex-col items-center justify-between  p-16">
                 <div
-                  className="h-30 flex h-64 w-full items-center justify-center px-2"
+                  className="h-30 flex h-96 w-full items-center justify-center px-2"
                   style={{
                     backgroundImage: `url(${blob})`,
                     backgroundRepeat: 'no-repeat',
@@ -26,17 +27,20 @@ function Home() {
                     backgroundPosition: 'center',
                   }}
                 >
-                  <p className="text-4xl font-semibold text-white">
+                  <p className="text-4xl font-semibold text-slate-100">
                     Welcome to Code School
                   </p>
                 </div>
                 <div className="text-center">
-                  <button className="w-32 rounded-lg bg-white p-4 font-semibold">
+                  <Link
+                    to={`/app`}
+                    className="bg-background text-text rounded-lg px-8 py-4 font-semibold transition-all hover:bg-green-700 hover:text-white"
+                  >
                     Enter
-                  </button>
+                  </Link>
                 </div>
               </div>
-              <p className="p-2 text-left text-sm">
+              <p className="text-text p-2 text-left text-sm">
                 © 2024 Marko Čabo. All rights reserved.
               </p>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,66 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
+        },
+        popover: {
+          DEFAULT: 'var(--popover)',
+          foreground: 'var(--popover-foreground)',
+        },
+        card: {
+          DEFAULT: 'var(--card)',
+          1: 'var(--card-1)',
+          2: 'var(--card-2)',
+          3: 'var(--card-3)',
+          4: 'var(--card-4)',
+        },
+        text: {
+          DEFAULT: 'var(--text)',
+        },
+        hover: {
+          DEFAULT: 'var(--hover)',
+          select: 'var(--hover-select)',
+        },
+        disabled: {
+          DEFAULT: 'var(--disabled)',
+          foreground: 'var(--disabled-foreground)',
+        },
+        success: {
+          DEFAULT: 'var(--success)',
+        },
+        borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 2px)',
+          sm: 'calc(var(--radius) - 4px)',
+        },
+      },
+    },
+  },
+  plugins: [],
 };
